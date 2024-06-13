0783404127e524c561d22009fa072b37d3564ea6
throv <trv.vieira@gmail.com>
2024-06-13 12:57:02 -0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('olá mundo');
+import fs from 'fs';
+import chalk from 'chalk';
+
+const treatError = (err) => {
+    throw new Error(chalk.red(err.code, 'Directory not found'));
+}
+
+const catchFile = (filePath) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            treatError(err);
+            return;
+        }
+        console.log(chalk.green(data));
+    })
+}
+
+
+catchFile('./arquivos/texto.md');
